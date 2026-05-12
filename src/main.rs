2d33f65fe2f4a0ee4a67272de0cc34b6e2c8b452
Staffan Bjornson <staffan.bjornson@toor.se>
2026-05-12 18:41:41 +0200

    Chapter 3 impl and self

--- src/main.rs
+++ src/main.rs
@@ -6,37 +6,28 @@ struct AgentConfig {
     tools_enabled: bool,
 }
 
-fn create_default_config() -> AgentConfig {
-    AgentConfig {
-        name: "OpenClaw Mini RS".to_string(),
-        model: "llama3.2:3b".to_string(),
-        temperature: 0.7,
-        max_tokens: 2048,
-        tools_enabled: true,
+impl AgentConfig {
+    fn default() -> AgentConfig {
+        AgentConfig {
+            name: "OpenClaw Mini RS".to_string(),
+            model: "llama3.2:3b".to_string(),
+            temperature: 0.7,
+            max_tokens: 2048,
+            tools_enabled: true,
+        }
+    }
+
+    fn print(&self) {
+        println!("Agent: {}", self.name);
+        println!("Agent: {}", self.model);
+        println!("Agent: {}", self.temperature);
+        println!("Agent: {}", self.max_tokens);
+        println!("Agent: {}", self.tools_enabled);
     }
 }
 
-fn print_config(config: &AgentConfig) {
-    println!("Agent Name: {}", config.name);
-    println!("Model: {}", config.model);
-    println!("Temperature: {}", config.temperature);
-    println!("Max Tokens: {}", config.max_tokens);
-    println!("Tools Enabled: {}", config.tools_enabled);
-}
-
-fn change_model(config: &mut AgentConfig, new_model: &str) {
-    config.model = new_model.to_string();
-}
-
 fn main() {
-    let mut config = create_default_config();
+    let config = AgentConfig::default();
 
-    println!("Before: ");
-    print_config(&config);
-
-    change_model(&mut config, "qwen2.5:7b");
-
-    println!();
-    println!("After: ");
-    print_config(&config);
+    config.print();
 }
