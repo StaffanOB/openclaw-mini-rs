c661bd98a4b005e37fb6a760f4de6c8a49242f95
Staffan Bjornson <staffan.bjornson@toor.se>
2026-05-18 21:12:41 +0200

    cloning

--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,50 @@
 
-struct AgentConfig {
-    name: String,
-    model: String,
-    temperature: f32,
-    max_tokens: u32,
-    tools_enabled: bool,
+struct Message {
+    role: String,
+    content: String,
 }
 
-impl AgentConfig {
-    fn default() -> Self {
+struct Session {
+    messages: Vec<Message>,
+}
+
+impl Session {
+    fn new() -> Self {
         Self {
-            name: "OpenClaw Mini RS".to_string(),
-            model: "llama3.2:3b".to_string(),
-            temperature: 0.7,
-            max_tokens: 2048,
-            tools_enabled: true,
+            messages: Vec::new(),
+        }
+    }
+    fn add_message(&mut self, role: &str, content: &str) {
+        let message = Message {
+            role: role.to_string(),
+            content: content.to_string(),
+        };
+
+        self.messages.push(message);
+    }
+
+    fn print_hitstory(&self) {
+        for message in &self.messages {
+            println!("{}: {}:", message.role, message.content);
         }
     }
 
-    fn print(&self) {
-        println!("Agent name: {}", self.name);
-        println!("  - Model: {}", self.model);
-        println!("  - Temperature: {}", self.temperature);
-        println!("  - Max Tokens: {}", self.max_tokens);
-        println!("  - Tools enabled: {}", self.tools_enabled);
+    fn last_message_content(&self) -> Option<&str> {
+        self.messages.last().map(|message| message.content.as_str())
     }
-
-    fn change_model(&mut self, new_model: &str) {
-        self.model = new_model.to_string();
-    }
-
-    fn summary(&self) -> String {
-       format!("{} using {}, with temperature {}, using max tokens of {}, and tools are set to {}.", self.name, self.model, self.temperature, self.max_tokens, self.tools_enabled) 
-    }
-
-    fn disable_tools(&mut self) {
-       self.tools_enabled = false; 
-    }
-
 }
 
 fn main() {
-    let mut config = AgentConfig::default();
+    let mut session = Session::new();
 
-    println!("Before: \n");
-    println!("{}", config.summary());
+    session.add_message("system", "You are OpenClaw MINI");
+    session.add_message("user", "Hello agent.");
+    session.add_message("assistant", "Hello! How can i help?");
 
-    config.change_model("qwuen2.5:7b");
-    config.disable_tools();
+    session.print_hitstory();
 
-    println!("\nAfter:\n");
-    println!("{}", config.summary());
+    if let Some(content) = session.last_message_content() {
+        println!();
+        println!("Last message was: {}", content);
+    }
 }
